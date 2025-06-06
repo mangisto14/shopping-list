c0d7f35516144f2f055fb890f88d3a10d4574b36
mangisto14 <160495661+mangisto14@users.noreply.github.com>
2025-06-06 15:29:52 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+console.log(`MONGO_UR ${process.env.MONGO_UR}`);
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, useUnifiedTopology: true,
 }).then(() => console.log("MongoDB connected"))
